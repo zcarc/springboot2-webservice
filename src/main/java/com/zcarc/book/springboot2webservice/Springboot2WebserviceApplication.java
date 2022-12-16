d9c53d03f49f418f8f469c968e809ef9ae663ce6
Hyunsoo Lee <wrkbrs@gmail.com>
2022-12-16 18:19:30 +0900

    first commit

---
+++ src/main/java/com/zcarc/book/springboot2webservice/Springboot2WebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.zcarc.book.springboot2webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot2WebserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot2WebserviceApplication.class, args);
+    }
+
+}
