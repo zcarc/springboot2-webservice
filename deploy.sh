e48a24b1a99b193acf0e40f022af47e35afaa1dc
Hyunsoo Lee <wrkbrs@gmail.com>
2022-12-19 20:23:04 +0900

    properties root 수정, deploy 수정

--- deploy.sh
+++ deploy.sh
@@ -42,6 +42,6 @@ echo "> JAR Name: $JAR_NAME"
 echo "> $JAR_NAME 실행"
 
 nohup java -jar \
-    -Dspring.config.location=classpath:/application.properties,/home/ec2-user/app/application-oauth.properties \
-    -Dspring.profiles.active=oauth \
+    -Dspring.config.location=classpath:/application.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties,classpath:/application-real.properties \
+    -Dspring.profiles.active=real \
     $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
